b47d67a59c1aa7b73a8ab830ffd6aec59dd21ea0
sesam <petersz@me.com>
2020-04-14 16:15:31 +0200

    left-right button functionality

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ let data = [
         photo: "./assets/gallery_2.jpg",
         title: "Trondheim",
         description: "Tradisjonelle hus ved siden av Nidelva." 
-    }
+    },
     {
         photo: "./assets/gallery_3.jpg",
         title: "Bergen",
@@ -26,3 +26,21 @@ function load_photo(id) {
 }
 
 load_photo(current);
+
+$('#arrow-left').click(function left(){
+    if (current === 0) {
+        current = data.length-1;
+    } else {
+        current--;
+    }
+    load_photo(current);
+});
+
+$('#arrow-right').click(function right(){
+    if (current === data.length-1) {
+        current = 0
+    } else {
+        current++;
+    }
+    load_photo(current);
+});
